7e8d063ff18d94007b62cf9c2d9454fe9fe725e5
shitcode6 <>
2020-10-28 16:20:46 -0400

    clean up

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import datetime
 import tensorflow as tf
 import cv2
 import pyvirtualcam
+from PIL import Image
+import numpy as np
+from threading import Thread
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.6
 session = tf.compat.v1.Session(config=config)
-from PIL import Image
-import numpy as np
+
 model = tf.keras.models.load_model("esrgan")
-from threading import Thread
-import cv2
+
 class WebcamVideoStream:
 	def __init__(self, src=0):
 		self.stream = cv2.VideoCapture(src)
@@ -30,12 +36,6 @@ class WebcamVideoStream:
 	def stop(self):
 		self.stopped = True
 
-
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import cv2
 cammode = True
 
 if cammode:
@@ -45,30 +45,20 @@ vs = WebcamVideoStream(src=0).start()
 while True:
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
-	ofrm = frame
-	frame = np.array(frame)
-	print(frame.shape)
-	low_resolution_image = frame
-	print("casting")
-	low_resolution_image = [frame.astype(np.float32)]
-	print("defining")
-	deff = np.array(low_resolution_image)
-	print("predicting")
+	deff = np.array([np.array(frame).astype(np.float32)])
 	super_resolution = model.predict(deff)
-	print("casting")
 	super_resolution = tf.cast(tf.clip_by_value(super_resolution[0], 0, 255), tf.uint8)
-	print("showing")
-	print(np.array(super_resolution).shape)
 	frmm = np.array(super_resolution)
 	if cammode:
 		b_channel, g_channel, r_channel = cv2.split(frmm)
 		alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50
-		img_BGRA = cv2.merge((r_channel, g_channel, b_channel, alpha_channel))
-		cam.send(img_BGRA)
+		img_RGBA = cv2.merge((r_channel, g_channel, b_channel, alpha_channel))
+		cam.send(img_RGBA)
 		cam.sleep_until_next_frame()
 	else:
 		cv2.imshow('frame',frmm)
 		cv2.waitKey(1)
+
 
 cv2.destroyAllWindows()
 vs.stop()
